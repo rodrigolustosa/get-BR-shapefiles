933e54b80b8bbe88cbb13cd26a100d572c922dd5
rodrigolustosa <rodrigo.lustosa@usp.br>
2024-01-09 12:11:41 -0300

    download files

--- get_br_shapefiles.R
+++ get_br_shapefiles.R
@@ -8,10 +8,28 @@
 # initialization ----------------------------------------------------------
 
 # packages
-# library(tidyverse)
+library(tidyverse)
+library(sf)
 library(geobr)
 
 # directory and file names
+dir_output <- "output"
+dir_immediate_region <- "immediate"
+dir_municip          <- "municip"
 
+# download shapefiles -----------------------------------------------------
+
+# geobr information
+geobr::list_geobr()
+
+# immediate regions
+rmsp_s <- read_immediate_region(code_immediate = 350001,)
+rmsp   <- read_immediate_region(code_immediate = 350001,simplified = F)
+
+# municipalities from metropolitan areas
+rmsp_municip_s <- read_metro_area() %>% 
+  filter(name_metro == "RM São Paulo")
+rmsp_municip <- read_metro_area(simplified = F) %>% 
+  filter(name_metro == "RM São Paulo")
 
 # ---------------------------------------------------------------------------- #
