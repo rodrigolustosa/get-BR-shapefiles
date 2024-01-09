89f85e541971c97eb3894826bbd01e4c7b9d9740
rodrigolustosa <rodrigo.lustosa@usp.br>
2024-01-09 12:27:40 -0300

    save shapefiles

--- get_br_shapefiles.R
+++ get_br_shapefiles.R
@@ -14,7 +14,7 @@ library(geobr)
 
 # directory and file names
 dir_output <- "output"
-dir_immediate_region <- "immediate"
+dir_immediate_region <- "metropolitan_region"
 dir_municip          <- "municip"
 
 # download shapefiles -----------------------------------------------------
@@ -31,5 +31,33 @@ rmsp_municip_s <- read_metro_area() %>%
   filter(name_metro == "RM São Paulo")
 rmsp_municip <- read_metro_area(simplified = F) %>% 
   filter(name_metro == "RM São Paulo")
+
+# save shapefiles ---------------------------------------------------------
+
+# create subdirectories
+dir_path <- file.path(dir_output,dir_immediate_region)
+if(!dir.exists(dir_path)) dir.create(dir_path)
+dir_path <- file.path(dir_output,dir_municip)
+if(!dir.exists(dir_path)) dir.create(dir_path)
+
+# Metropolitan Region of Sao Paulo
+dir_path <- file.path(dir_output,dir_immediate_region,"RMSP")
+if(!dir.exists(dir_path)) dir.create(dir_path)
+st_write(rmsp, file.path(dir_path,"RMSP.shp"))
+
+# Metropolitan Region of Sao Paulo (simplified)
+dir_path <- file.path(dir_output,dir_immediate_region,"RMSP_s")
+if(!dir.exists(dir_path)) dir.create(dir_path)
+st_write(rmsp_s, file.path(dir_path,"RMSP_s.shp"))
+
+# municipalities from the Metropolitan Region of Sao Paulo
+dir_path <- file.path(dir_output,dir_municip,"RMSP_municip")
+if(!dir.exists(dir_path)) dir.create(dir_path)
+st_write(rmsp_municip, file.path(dir_path,"RMSP_municip.shp"))
+
+# municipalities from the Metropolitan Region of Sao Paulo (simplified)
+dir_path <- file.path(dir_output,dir_municip,"RMSP_municip_s")
+if(!dir.exists(dir_path)) dir.create(dir_path)
+st_write(rmsp_municip_s, file.path(dir_path,"RMSP_municip_s.shp"))
 
 # ---------------------------------------------------------------------------- #
